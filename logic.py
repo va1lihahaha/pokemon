2f63469aff903ae30927c05de2170c0339778880
ваше имя <ivanartemenko@bk.ru>
2024-11-09 22:34:56 +0400

    Комментарий к коммиту

--- logic.py
+++ logic.py
@@ -11,6 +11,8 @@ class Pokemon:
         self.pokemon_number = randint(1,1000)
         self.img = self.get_img()
         self.name = self.get_name()
+        self.hp = randint(200,400)
+        self.power = randint(30, 50)
 
         Pokemon.pokemons[pokemon_trainer] = self
 
@@ -41,11 +43,53 @@ class Pokemon:
 
     # Метод класса для получения информации
     def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+        return f"Имя твоего покеомона: {self.name}\nЗдоровье: {self.hp}\nАтака:{self.power}"
+    
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = randint(1,3)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+
+
+
+
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}.\n"+\
+                f"@{self.pokemon_trainer} нанёс {self.power} урона.\n"+\
+                f"Здоровье @{enemy.pokemon_trainer} {enemy.hp}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
 
     # Метод класса для получения картинки покемона
     def show_img(self):
         return self.img
+class Wizard(Pokemon):
+    def info(self):
+        return super().info() +"\nУ тебя покемон волшыбник"
+    
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power= randint(5,15)
+        self.power += super_power
+        result= super().attack(enemy)
+        self.power-=super_power
+        return "была пременина супер сила\n"+result
+    
+    
+pok_1=Fighter("123")
+print(pok_1.info())
+pok_2=Wizard("12334")
+print(pok_2.info())
+print(pok_1.attack(pok_2))
+print(pok_1.attack(pok_2))
+print(pok_1.attack(pok_2))
+print(pok_1.attack(pok_2))
+print(pok_1.attack(pok_2))
+    
 
 
 
